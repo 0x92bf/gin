9665e03b08f52d2ecc6ac927c72bc2b5af9bde86
natural <312125539@qq.com>
2019-03-15 17:34:26 +0800

    Delete help.go

--- help.go
+++
@@ -1,9 +0,0 @@
-package common
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func ResponJson(c *gin.Context,msg string,data interface{},errorCode interface{})  {
-	c.JSON(200,gin.H{"msg":msg,"data":data,"errorCode":errorCode})
-}
